4ba38c642c47f1fc10cb130269598d472986c7af
David A <57709284+Hxlvlra@users.noreply.github.com>
2021-01-12 10:37:06 +0800

    refactor: modularize the routing mechanism for this project

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/**
+ * initialize all the routes
+ *
+ * @param {*} app
+ */
+exports.routes = (app) => {
+    // access root address - http://localhost/
+    app.get('/', {
+      /**
+       * handles the request for a given route
+       */
+      handler: async (req) => {
+        // this is the response in JSON format
+        return { success: true }
+      }
+    });
+  }
+  
