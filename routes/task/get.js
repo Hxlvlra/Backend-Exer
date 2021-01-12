a2e414bf22b3eee07bce7c40ec05ca0a8091b390
David A <57709284+Hxlvlra@users.noreply.github.com>
2021-01-12 16:42:52 +0800

    feat: add a 404 if no existing id is given

--- routes/task/get.js
+++ routes/task/get.js
@@ -11,8 +11,10 @@ exports.get = app => {
    * This gets one task from the database given a unique ID
    *
    * @param {import('fastify').FastifyRequest} request
+   * @param {import('fastify').FastifyReply<Response>} response
    */
-  app.get('/task/:id', (request) => {
+  app.get('/task/:id', (request, response) => {
+
     const { params } = request;
     const { id } = params;
 
@@ -21,6 +23,16 @@ exports.get = app => {
     const tasks = getTasks(filename, encoding);
 
     const index = tasks.findIndex(task => task.id === id);
+
+    if (index < 0) {
+      return response
+        .code(404)
+        .send({
+          success: false,
+          code: 'task/not-found',
+          message: 'Task doesn\'t exist'
+        });
+    }
 
     const data = tasks[index];
 
