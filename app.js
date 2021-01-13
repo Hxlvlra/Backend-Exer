475edfa2dc141924599ba2c5df7e405fd858583b
David A <57709284+Hxlvlra@users.noreply.github.com>
2021-01-13 18:59:43 +0800

    feat: add registration of jwt

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const fastify = require('fastify');
 const swagger = require('fastify-swagger');
 const sensible = require('fastify-sensible');
+const jwt = require('fastify-jwt');
+const { readFileSync } = require('fs');
 const { errorHandler } = require('./error-handler.js');
 const { definitions } = require ('./definitions')
 const { routes } = require('./routes');
 const { connect } = require('./db');
 const { name: title, description, version } = require ('./package.json');
+
+const audience = 'this-audience';
+const issuer = 'localhost';
 
 /**
  * This is the function to call to initialize the server
@@ -16,6 +21,24 @@ const { name: title, description, version } = require ('./package.json');
 exports.build = async (opts = { logger: false, trustProxy: false }) => {
   // initialize our server using Fastify
   const app = fastify(opts);
+
+  app.register(jwt, {
+    secret: {
+      private: readFileSync('./cert/keyfile', 'utf8'),
+      public: readFileSync('./cert/keyfile.key.pub', 'utf8')
+    },
+    sign: {
+      algorithm: 'RS256',
+      audience,
+      issuer,
+      expiresIn: '1h'
+    },
+    verify: {
+      audience,
+      issuer
+    }
+  });
+
 
   app.register(sensible).after(() => {
     app.setErrorHandler(errorHandler);
